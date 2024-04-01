2b00ad64b7d589772a6486826912f127cda8192e
Jordan Olsen <2001jordanolsen@gmail.com>
2024-03-31 18:23:42 -0600

    missing some files

---
+++ javaScipt/home.js
@@ -0,0 +1,21 @@
+const body = document.getElementById("body");
+
+const schoolBlock = document.getElementById("school-block");
+schoolBlock.addEventListener("click", () => {
+    window.location.href = "schooling.html";
+});
+
+const aboutBlock = document.getElementById("about-block");
+aboutBlock.addEventListener("click", () => {
+    window.location.href = "about.html";
+});
+
+const projectsBlock = document.getElementById("projects-block");
+projectsBlock.addEventListener("click", () => {
+    window.location.href = "projects.html";
+});
+
+const experieneBlock = document.getElementById("experience-block");
+experieneBlock.addEventListener("click", () => {
+    window.location.href = "experience.html";
+});
